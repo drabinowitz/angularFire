d18c0af3f59e85eb4f5e83dd0813eaa780d6ec00
git-username <git-email-address>
2014-08-10 22:19:53 -0400

    added firebase functionality

--- app/app.js
+++ app/app.js
@@ -1,7 +1,23 @@
 var app = angular.module('angularfire',['firebase']);
 
-app.controller('fireCtrl',['$firebase',function($firebase){
+app.controller('fireCtrl',['$scope','$firebase',function($scope,$firebase){
 
-	this.fireVal = 'sometext';
+	var ref = new Firebase('https://burning-fire-8322.firebaseio.com');
+
+	var sync = $firebase(ref);
+
+	var syncObject = sync.$asObject();
+
+	syncObject.$bindTo($scope, "fireBindings");
+
+	var childRef = new Firebase('https://burning-fire-8322.firebaseio.com').child('fireVals');
+
+	$scope.fireVals = $firebase(childRef).$asObject();
+
+/*	this.savefireVal = function(){
+
+		$scope.fireVals.$save();
+
+	};*/
 
 }]);
